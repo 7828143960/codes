4948f52d9554b88e660ff39db0f9f40ed5500b85
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-08-07 16:08:15 +0530

    Create output.tf

---
+++ Jenkinsfile/Terraform/output.tf
@@ -0,0 +1,12 @@
+output "eks_cluster_name" {
+  value = aws_eks_cluster.my_cluster.name
+}
+output "eks_role_name" {
+  value = aws_iam_role.eks_cluster.name
+}
+output "eks_node_group_name" {
+  value = aws_eks_node_group.my_node_group.node_group_name
+}
+output "ecr_repository_url" {
+  value = aws_ecr_repository.my_ecr_repo.repository_url
+}
