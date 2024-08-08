066b7ff25521f834a336450e73089ff755c9264e
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-08-08 13:43:50 +0530

    Update variables.tf

--- Jenkinsfile/Terraform/variables.tf
+++ Jenkinsfile/Terraform/variables.tf
@@ -12,7 +12,7 @@ variable "networking" {
     cidr_block      = "10.0.0.0/16"
     region          = "us-east-1"
     vpc_name        = "custom-vpc"
-    azs             = ["us-east-1a", "us-east-1b"]
+    azs             = ["eu-north-1a", "eu-north-1b"]
     public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
     private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
     nat_gateways    = true
