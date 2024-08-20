4dc6c5914dd08b0ab54cb84891eff5a930e5ebd7
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-08-20 16:17:57 +0530

    Create destroy.groovy

---
+++ src/terraform/destroy.groovy
@@ -0,0 +1,20 @@
+
+package src.terraform
+
+def call(String rootPath, String childPath, String ACTION) {
+   // stage('Approval For Destroy') {
+     //   script {
+        //    if (ACTION == 'destroy') {
+                // input "Do you want to Destroy Terraform Infrastructure?"
+          //  }
+       // }
+   // }
+
+    if (ACTION == 'destroy') {
+        stage('Terraform destroy') {
+            script {
+                sh "cd ${rootPath}/${childPath} && terraform destroy -auto-approve"
+            }
+        }
+    }
+}
