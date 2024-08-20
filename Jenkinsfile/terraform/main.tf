b17ac85c5f6cc2968a4701b94ee73c66adc4c876
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-08-20 20:00:16 +0530

    Update main.tf

--- Jenkinsfile/terraform/main.tf
+++ Jenkinsfile/terraform/main.tf
@@ -39,4 +39,20 @@ resource "aws_security_group" "example" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+*------------Generate SSH Key--------------*/
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+ }
+/*----------Create PEM Key----------------------*/
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+ }
+/*----------Download PEM Key-------------------*/
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = var.key_name
+ }
 }
