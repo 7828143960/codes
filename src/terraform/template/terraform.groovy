9a6b3955e4ad40554af8f0546a00685a9df8fc12
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-08-20 16:25:22 +0530

    Create terraform.groovy

---
+++ src/terraform/template/terraform.groovy
@@ -0,0 +1,21 @@
+package src.terraform.template
+
+import src.terraform.*
+
+def call(String url, String creds, String branch, String rootPath, String childPath, String ACTION){
+
+    checkout = new gitcheckout()
+    init = new tfinit()
+    validate = new tfvalidate()
+    fmt = new tffmt()
+    apply = new tfapply()
+    
+    
+  
+    gitcheckout.call(url, creds, branch)
+    tfinit.call(rootPath, childPath)
+    tfvalidate.call(rootPath, childPath)
+    tffmt.call(rootPath, childPath, ACTION)
+    tfapply.call(rootPath, childPath, ACTION)
+    
+}
