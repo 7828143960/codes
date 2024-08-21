e8698388ac09e17f11ddf491fc5ed452010958f4
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-08-21 12:11:01 +0530

    Update terraform.groovy

--- src/org/template/terraform.groovy
+++ src/org/template/terraform.groovy
@@ -2,7 +2,7 @@ package org.template
 
 import org.terraform.*
 
-def call(String url, String creds, String branch, String rootPath, String childPath, String ACTION){
+def call(String url, String creds, String branch, String rootPath, String childPath){
 
     gitcheckout = new checkout()
     tfinit = new init()
@@ -16,6 +16,6 @@ def call(String url, String creds, String branch, String rootPath, String childP
     tfinit.call(rootPath, childPath)
     tfvalidate.call(rootPath, childPath)
     tffmt.call(rootPath, childPath)
-    tfapply.call(rootPath, childPath, ACTION)
+    tfapply.call(rootPath, childPath)
     
 }
