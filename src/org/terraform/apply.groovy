4e343ebd351ad42535bb2b1841fa897327ea08b5
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-08-21 11:18:01 +0530

    Update apply.groovy

--- src/org/terraform/apply.groovy
+++ src/org/terraform/apply.groovy
@@ -1,11 +1,19 @@
 package org.terraform
 
-def call(String rootPath, String childPath) {
+def call(String rootPath, String childPath, string ACTION) {
     stage("Terraform Plan") {
         script {
             sh "cd ${rootPath}/${childPath} && terraform plan"
         }
     }
+    
+    if (ACTION == 'apply') {
+        stage('Approval For Apply') {
+            script {
+                // Prompt for approval before applying changes
+                input "Do you want to apply Terraform changes?"
+            }
+        }
 
     stage('Terraform Apply') {
         script {
