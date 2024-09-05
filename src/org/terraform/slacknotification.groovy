a988e3198b04e3fe76af7ac20630c55c47a9562e
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-09-05 16:52:23 +0530

    Update slacknotification.groovy

--- src/org/terraform/slacknotification.groovy
+++ src/org/terraform/slacknotification.groovy
@@ -1,12 +1,12 @@
 package org.terraform
 
 def call() {
- stage(Slack Notification) {  
-    def status = env.BUILD_STATUS ?: 'SUCCESS' // Default to 'SUCCESS' if BUILD_STATUS is not set
-    if (status == 'FAILURE') {
-        slackSend channel: 'jenkinss', message: 'Job Failed'
-    } else if (status == 'SUCCESS') {
-        slackSend channel: 'jenkinss', message: 'Job Build successfully'
+    stage('Slack Notification') {  
+        def status = env.BUILD_STATUS ?: 'SUCCESS' // Default to 'SUCCESS' if BUILD_STATUS is not set
+        if (status == 'FAILURE') {
+            slackSend channel: 'jenkinss', message: 'Job Failed'
+        } else if (status == 'SUCCESS') {
+            slackSend channel: 'jenkinss', message: 'Job Build successfully'
+        }
     }
-}
 }
