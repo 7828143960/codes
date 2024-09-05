aa515ad2bc8358d0631a3c6046da06c268689023
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-09-05 16:03:46 +0530

    Update terraform.groovy

--- src/org/template/terraform.groovy
+++ src/org/template/terraform.groovy
@@ -7,15 +7,15 @@ def call(String url, String creds, String branch, String rootPath, String childP
     gitcheckout = new checkout()
     tfinit = new init()
     tfvalidate = new validate()
-    // tffmt = new fmt()
     tfapply = new apply()
+    notification = new slack-notification()
     
     
   
     gitcheckout.call(url, creds, branch)
     tfinit.call(rootPath, childPath)
     tfvalidate.call(rootPath, childPath)
-    // tffmt.call(rootPath, childPath)
     tfapply.call(rootPath, childPath)
+    notification.call(rootPath, childPath)
     
 }
