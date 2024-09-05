affed5356aef68ef9520243b407e82f9d4aa62a0
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-09-05 16:46:11 +0530

    Update terraform.groovy

--- src/org/template/terraform.groovy
+++ src/org/template/terraform.groovy
@@ -16,6 +16,6 @@ def call(String url, String creds, String branch, String rootPath, String childP
     tfinit.call(rootPath, childPath)
     tfvalidate.call(rootPath, childPath)
     tfapply.call(rootPath, childPath)
-    notification.call(rootPath, childPath)
+    notification.call()
     
 }
