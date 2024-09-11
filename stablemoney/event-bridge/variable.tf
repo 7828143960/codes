6866e46a92aace594241f44f166a8ce4e68ded1f
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-09-11 20:05:40 +0530

    Update variable.tf

--- stablemoney/event-bridge/variable.tf
+++ stablemoney/event-bridge/variable.tf
@@ -1,7 +1,10 @@
+# variables.tf
+
 variable "region" {
   description = "AWS region to deploy resources"
   type        = string
 }
+
 variable "eventbridge_rules" {
   description = "List of EventBridge rules with Lambda ARNs"
   type = list(object({
@@ -10,11 +13,28 @@ variable "eventbridge_rules" {
     lambda_arn          = string
     description         = string
   }))
-  default = []
 }
 
 variable "tags" {
   description = "Tags to assign to the resources"
   type        = map(string)
+}
+
+variable "lambda_permission_statement_id_prefix" {
+  description = "Prefix for the Lambda permission statement IDs"
+  type        = string
+  default     = "AllowExecutionFromEventBridge"
+}
+
+variable "lambda_target_id_prefix" {
+  description = "Prefix for the Lambda target IDs"
+  type        = string
+  default     = "lambda_target"
+}
+
+variable "input_transformation" {
+  description = "Optional input transformation to the Lambda function"
+  type        = map(string)
+  default     = {}
 }
 
