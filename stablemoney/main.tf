d986780c106ed84c388f48f9b02ed58036052192
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-09-11 20:15:39 +0530

    Update main.tf

--- stablemoney/main.tf
+++ stablemoney/main.tf
@@ -1,7 +1,10 @@
 module "eventbridge_scheduler" {
   source = "./event-bridge"
 
-  region            = var.region
-  eventbridge_rules = var.eventbridge_rules
-  tags              = var.tags
+  region                                = var.region
+  eventbridge_rules                     = var.eventbridge_rules
+  tags                                  = var.tags
+  lambda_permission_statement_id_prefix = var.lambda_permission_statement_id_prefix
+  lambda_target_id_prefix               = var.lambda_target_id_prefix
+  input_transformation                  = var.input_transformation
 }
