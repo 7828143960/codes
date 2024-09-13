adb64b8318ec1b3bda6c402384bc33914449801c
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-09-13 22:28:24 +0530

    Update terraform.groovy

--- src/org/template/terraform.groovy
+++ src/org/template/terraform.groovy
@@ -30,31 +30,25 @@ def call(String url, String creds, String branch, String rootPath, String childP
     tfapply = new apply()
     notification = new slacknotification()
 
-    // Keep track of whether an exception occurs
-    def aborted = false
+    def buildStatus = 'SUCCESS'  // Default to SUCCESS
 
     try {
         gitcheckout.call(url, creds, branch)
         tfinit.call(rootPath, childPath)
         tfvalidate.call(rootPath, childPath)
         tfapply.call(rootPath, childPath)
-        // Set SUCCESS if all steps are successful
-        currentBuild.result = 'SUCCESS'
     } catch (Exception e) {
-        // Set FAILURE if an exception occurs
-        currentBuild.result = 'FAILURE'
+        buildStatus = 'FAILURE'
         throw e
     } finally {
-        // Check if the build was aborted
-        if (currentBuild.result == null || currentBuild.result == 'ABORTED') {
-            aborted = true
-            currentBuild.result = 'ABORTED'
+        // Check if the build is aborted
+        if (currentBuild.result == 'ABORTED') {
+            buildStatus = 'ABORTED'
         }
-        
+        currentBuild.result = buildStatus
         // Send Slack notification
         notification.call()
-        
-        // Debug output for tracking
-        echo "Build result in finally block: ${currentBuild.result}"
+        // Debug output
+        echo "Final build result in finally block: ${buildStatus}"
     }
 }
