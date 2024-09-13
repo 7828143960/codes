b2f8418559c8a8ce7ea951141ccc57be9e03f506
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-09-13 17:26:06 +0530

    Update slacknotification.groovy

--- src/org/terraform/slacknotification.groovy
+++ src/org/terraform/slacknotification.groovy
@@ -21,46 +21,48 @@ package org.terraform
 
 def call() {
     stage('Slack Notification') {  
-        // Use currentBuild.currentResult to get the build status
-        def status = currentBuild.currentResult ?: 'SUCCESS'
+        // Use currentBuild.result to get the build status reliably
+        def status = currentBuild.result ?: 'SUCCESS'
         
         def branchName = params.branch ?: 'main'
         def jobStartTime = new Date(currentBuild.startTimeInMillis).format("yyyy-MM-dd HH:mm:ss", TimeZone.getTimeZone('GMT'))
         
         def message
         
+        // Construct the message based on the build status
         if (status == 'FAILURE') {
             message = "Job Failed on branch ${branchName} at ${jobStartTime} GMT."
         } else if (status == 'SUCCESS') {
-            message = """
-            Job Build successfully on branch ${branchName} at ${jobStartTime} GMT.
-            """
+            message = "Job Build successfully on branch ${branchName} at ${jobStartTime} GMT."
         } else {
             message = "Job Status: ${status} on branch ${branchName} at ${jobStartTime} GMT."
         }
         
-        // Determine the color based on build status
+        // Determine the Slack message color based on build status
         def color
-        if (status == 'SUCCESS') {
-            color = "good"
-        } else if (status == 'FAILURE') {
-            color = "danger"
-        } else if (status == 'ABORTED') {
-            color = "warning"
-        } else {
-            color = "warning"
+        switch (status) {
+            case 'SUCCESS':
+                color = "good"
+                break
+            case 'FAILURE':
+                color = "danger"
+                break
+            case 'ABORTED':
+                color = "warning"
+                break
+            default:
+                color = "warning"
         }
 
-        // Send Slack message
-        slackSend channel: 'jenkinss',
+        // Send the Slack notification
+        slackSend channel: 'jenkinss', // Ensure the channel name is correct
             color: color,
             message: """
             ${message}
-            Find Status of Pipeline: ${currentBuild.currentResult}
+            Find Status of Pipeline: ${status}
             Job Name: ${env.JOB_NAME}
             Build Number: ${env.BUILD_NUMBER}
             Build URL: ${env.BUILD_URL}
             """
     }
 }
-
