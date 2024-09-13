943833ced1d7bcfc29bfcb22efb2d77ade3c3371
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-09-13 21:56:49 +0530

    Update terraform.groovy

--- src/org/template/terraform.groovy
+++ src/org/template/terraform.groovy
@@ -1,21 +1,46 @@
+// package org.template
+
+// import org.terraform.*
+
+// def call(String url, String creds, String branch, String rootPath, String childPath){
+
+//     gitcheckout = new checkout()
+//     tfinit = new init()
+//     tfvalidate = new validate()
+//     tfapply = new apply()
+//     notification = new slacknotification()
+    
+    
+  
+//     gitcheckout.call(url, creds, branch)
+//     tfinit.call(rootPath, childPath)
+//     tfvalidate.call(rootPath, childPath)
+//     tfapply.call(rootPath, childPath)
+//     notification.call()
+    
+// }
 package org.template
 
 import org.terraform.*
 
 def call(String url, String creds, String branch, String rootPath, String childPath){
-
     gitcheckout = new checkout()
     tfinit = new init()
     tfvalidate = new validate()
     tfapply = new apply()
     notification = new slacknotification()
-    
-    
-  
-    gitcheckout.call(url, creds, branch)
-    tfinit.call(rootPath, childPath)
-    tfvalidate.call(rootPath, childPath)
-    tfapply.call(rootPath, childPath)
-    notification.call()
-    
+
+    try {
+        gitcheckout.call(url, creds, branch)
+        tfinit.call(rootPath, childPath)
+        tfvalidate.call(rootPath, childPath)
+        tfapply.call(rootPath, childPath)
+        currentBuild.result = 'SUCCESS'
+    } catch (Exception e) {
+        currentBuild.result = 'FAILURE'
+        throw e
+    } finally {
+        notification.call()
+    }
 }
+
