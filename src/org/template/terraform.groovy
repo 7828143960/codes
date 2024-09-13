a50a1142b4b7ffcb3dcb60f275669906768a0be4
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-09-13 22:16:51 +0530

    Update terraform.groovy

--- src/org/template/terraform.groovy
+++ src/org/template/terraform.groovy
@@ -35,12 +35,16 @@ def call(String url, String creds, String branch, String rootPath, String childP
         tfinit.call(rootPath, childPath)
         tfvalidate.call(rootPath, childPath)
         tfapply.call(rootPath, childPath)
+        // Only set SUCCESS if all steps are successful
         currentBuild.result = 'SUCCESS'
     } catch (Exception e) {
-        currentBuild.result = 'FAILURE'
+        // Set FAILURE if an exception occurs
+        if (currentBuild.result != 'ABORTED') {
+            currentBuild.result = 'FAILURE'
+        }
         throw e
     } finally {
+        // This block will execute whether the build succeeds, fails, or is aborted
         notification.call()
     }
 }
-
