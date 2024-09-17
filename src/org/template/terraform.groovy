98c11a1b2535d080803c03da66209984e6654e87
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-09-17 13:30:46 +0530

    Update terraform.groovy

--- src/org/template/terraform.groovy
+++ src/org/template/terraform.groovy
@@ -1,31 +1,3 @@
-// package org.template
-
-// import org.terraform.*
-
-// def call(String url, String creds, String branch, String rootPath, String childPath) {
-//     gitcheckout = new checkout()
-//     tfinit = new init()
-//     tfvalidate = new validate()
-//     tfapply = new apply()
-//     notification = new slacknotification()
-
-//     try {
-//         gitcheckout.call(url, creds, branch)
-//         tfinit.call(rootPath, childPath)
-//         tfvalidate.call(rootPath, childPath)
-//         tfapply.call(rootPath, childPath)
-//         currentBuild.result = 'SUCCESS'
-//     } catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException e) {
-//         currentBuild.result = 'ABORTED'
-//         throw e
-//     } catch (Exception e) {
-//         currentBuild.result = 'FAILURE'
-//         throw e
-//     } finally {
-//         notification.call()
-//     }
-// }
-
 package org.template
 
 import org.terraform.*
