f54b6fa9884771d102d6f283c7ec91962c525454
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-09-17 15:48:14 +0530

    Update slacknotification.groovy

--- src/org/terraform/slacknotification.groovy
+++ src/org/terraform/slacknotification.groovy
@@ -1,69 +1,13 @@
-// package org.terraform
-
-// def call() {
-//     stage('Slack Notification') {
-//         def status = currentBuild.currentResult ?: 'SUCCESS'
-//         def branchName = params.branch ?: 'main'
-//         def jobStartTime = new Date(currentBuild.startTimeInMillis).format("yyyy-MM-dd HH:mm:ss", TimeZone.getTimeZone('GMT'))
-//         def message
-
-//         // Construct the message based on the build status
-//         if (status == 'FAILURE') {
-//             message = """
-//             Job Build Failed on branch ${branchName} at ${jobStartTime} GMT.
-//             """
-//         } else if (status == 'SUCCESS') {
-//             message = """
-//             Job Build successfully on branch ${branchName} at ${jobStartTime} GMT.
-//             """
-//         } else if (status == 'ABORTED') {
-//             message = """
-//             Job Build was aborted on branch ${branchName} at ${jobStartTime} GMT.
-//             """
-//         } else {
-//             message = """
-//             Job Build status is ${status} on branch ${branchName} at ${jobStartTime} GMT.
-//             """
-//         }
-
-//         // Determine the color based on build status
-//         def color
-//         if (status == 'SUCCESS') {
-//             color = "good"
-//         } else if (status == 'FAILURE') {
-//             color = "danger"
-//         } else if (status == 'ABORTED') {
-//             color = "warning"
-//         } else {
-//             color = "warning"
-//         }
-
-//         slackSend channel: 'jenkinss',
-//             color: color,
-//             message: """
-//             ${message}
-//             Find Status of Pipeline: ${status}
-//             Job Name: ${env.JOB_NAME}
-//             Build Number: ${env.BUILD_NUMBER}
-//             Build URL: ${env.BUILD_URL}
-//             BUILD_USER: ${env.BUILD_USER}
-//             """
-//     }
-// }
-
 package org.terraform
 
 def call() {
     stage('Slack Notification') {
         def status = currentBuild.currentResult ?: 'SUCCESS'
         def branchName = params.branch ?: 'main'
+        def userName = env.BUILD_USER ? env.BUILD_USER : 'User'
         def jobStartTime = new Date(currentBuild.startTimeInMillis).format("yyyy-MM-dd HH:mm:ss", TimeZone.getTimeZone('GMT'))
         def message
-
-        // Retrieve build user information
-       // def userName = env.BUILD_USER ? env.BUILD_USER : ''
-
-        // Construct the message based on the build status
+        
         if (status == 'FAILURE') {
             message = """
             Job Build Failed on branch ${branchName} at ${jobStartTime} GMT.
@@ -101,9 +45,8 @@ def call() {
             Find Status of Pipeline: ${status}
             Job Name: ${env.JOB_NAME}
             Build Number: ${env.BUILD_NUMBER}
-            Build URL: ${env.BUILD_URL}
-           // BUILD_USER: ${userName}
-            BUILD_USER: ${env.BUILD_USER}
+            Build url: ${env.BUILD_URL}
+            Build username: ${userName}
             """
     }
 }
