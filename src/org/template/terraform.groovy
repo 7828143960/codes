e52f9765349d04f8ea06f9a355da800b8a4ab073
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-09-17 13:22:11 +0530

    Update terraform.groovy

--- src/org/template/terraform.groovy
+++ src/org/template/terraform.groovy
@@ -1,3 +1,31 @@
+// package org.template
+
+// import org.terraform.*
+
+// def call(String url, String creds, String branch, String rootPath, String childPath) {
+//     gitcheckout = new checkout()
+//     tfinit = new init()
+//     tfvalidate = new validate()
+//     tfapply = new apply()
+//     notification = new slacknotification()
+
+//     try {
+//         gitcheckout.call(url, creds, branch)
+//         tfinit.call(rootPath, childPath)
+//         tfvalidate.call(rootPath, childPath)
+//         tfapply.call(rootPath, childPath)
+//         currentBuild.result = 'SUCCESS'
+//     } catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException e) {
+//         currentBuild.result = 'ABORTED'
+//         throw e
+//     } catch (Exception e) {
+//         currentBuild.result = 'FAILURE'
+//         throw e
+//     } finally {
+//         notification.call()
+//     }
+// }
+
 package org.template
 
 import org.terraform.*
@@ -22,6 +50,13 @@ def call(String url, String creds, String branch, String rootPath, String childP
         currentBuild.result = 'FAILURE'
         throw e
     } finally {
-        notification.call()
+        // Ensure the notification is called with required parameters
+        notification.call([
+            MESSAGE: "Build ${currentBuild.result} for branch ${branch}",
+            CHANNEL: "jenkinss",
+            DOMAIN: "demoworkspace-6868926",
+            TOKEN_ID: "slack"
+        ])
     }
 }
+
