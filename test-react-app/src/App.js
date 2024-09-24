ab23b34ce997798a4057e6b52453eff266e239c9
7828143960 <jaiswalshreya37@gmail.com>
2024-09-24 14:59:32 +0530

    adding react test code

---
+++ test-react-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello, this is a test React application for the Jenkins pipeline.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
