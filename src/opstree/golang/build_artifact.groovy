c81490a978b06c799cb9d6188374572c7d15adf7
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2024-10-14 16:13:16 +0530

    Update build_artifact.groovy

--- src/opstree/golang/build_artifact.groovy
+++ src/opstree/golang/build_artifact.groovy
@@ -6,8 +6,7 @@ def build_factory(Map step_params) {
     logger = new logger()
     if (step_params.perform_code_build == 'true') {
         build_artifact(step_params)
-    }
-    else {
+    } else {
         logger.logger('msg':'No valid option selected for Building Artifact. Please mention correct values.', 'level':'WARN')
     }
 }
@@ -21,12 +20,18 @@ def build_artifact(Map step_params) {
     repo_url = "${step_params.repo_url}"
     source_code_path = "${step_params.source_code_path}"
 
+    // Fetch the repository name using the parser and build the full path
     repo_dir = parser.fetch_git_repo_name('repo_url':"${repo_url}")
+    def full_source_path = "${env.WORKSPACE}/${source_code_path}/${repo_dir}"
 
-    dir("${WORKSPACE}/${repo_dir}") {
+    // Print debug information for verification
+    logger.logger('msg':"Full source path: ${full_source_path}", 'level':'DEBUG')
+
+    dir("${full_source_path}") {
         def build_command = "go mod tidy && go build -o ${step_params.output_binary}"
 
-        sh """ docker run --rm -v ~/.go:/go -v /var/lib/jenkins/workspace/eks/OT-Microservices/employee:/app -w /app golang:1.19 sh -c "${build_command}" """
+        // Use the dynamically constructed full_source_path for Docker volume mounting
+        sh """ docker run --rm -v ~/.go:/go -v ${full_source_path}:/app -w /app golang:1.19 sh -c "${build_command}" """
         logger.logger('msg':'Build successful', 'level':'INFO')
     }
 }
